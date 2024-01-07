d4d61162dd735422e5ef0b77055dceb5e67f08a9
DYLAND_MT\dyland_mt <dyland1209apple@gmail.com>
2024-01-07 17:14:37 -0600

    Logic added to remove album title -  first integration

--- BSWHRReactNativeDMT/views/home/HomeView.tsx
+++ BSWHRReactNativeDMT/views/home/HomeView.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 import HomeStyles from "./HomeStyles";
 import UsersList from "../../components/albumeslist/UsersList";
 import { UserInformation } from "../../models/UserInformation";
+import { TouchableOpacity } from "react-native";
 
 interface Props extends StackScreenProps<RootStackParamList,"HomeView"> {};
 
@@ -17,31 +18,43 @@ export const HomeView = ({navigation, route}: Props) => {
       albumes: ["1","2","3"]
     },
     {
-      name : "Juan",
-      albumes: ["1","2","3"]
-    },
-    {
-      name : "Juan",
-      albumes: ["1","2","3"]
-    },
-    {
       name : "Pedro",
+      albumes: ["1","2","3"]
+    },
+    {
+      name : "Pablo",
+      albumes: ["1","2","3"]
+    },
+    {
+      name : "Carnal",
       albumes: ["1","2","3","4"]
     },
     {
-      name : "Pedro",
+      name : "Hermano",
       albumes: ["1","2"]
     },
   ]
 
+  const [usersDataState, setUsersDataState] = useState(usersData)
+
+
+  function handleRemove(name:string, albumId:string) {
+    const userFiltered:UserInformation = usersData.find((userData) => userData.name === name) as UserInformation
+    const listUpdated = userFiltered.albumes.filter((album) => album!==albumId)
+    const userDataUpdated = usersDataState.map(userData =>  userData.name === name?{...userData,"albumes":listUpdated}:userData)
+    setUsersDataState(userDataUpdated)
+  }
     return (
         <SafeAreaView style={backgroundStyle}>
         <View>
           <Text style={HomeStyles.loginTitle}>
             Home View
           </Text>
+          <TouchableOpacity onPress={() => handleRemove("Hermano","1")}>
+            <Text>Click me</Text>
+          </TouchableOpacity>
         </View>
-        <UsersList usersData={usersData}/>
+        <UsersList usersData={usersDataState}/>
       </SafeAreaView>
     );
 };
